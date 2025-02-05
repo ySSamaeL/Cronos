77e9269674b82e5b7ccb9bedefe546db7df20b5f
Samuel Pereira <124547921+ySSamaeL@users.noreply.github.com>
2025-02-05 03:25:33 -0300

    Update cronometro.py

--- cronometro.py
+++ cronometro.py
@@ -3,8 +3,8 @@ from kivy.clock import Clock
 class Cronometro:
     def __init__(self):
         self.tempo_total = 0
-        self.segundos = 0
         self.evento = None
+        self.display_widget = None
 
     def iniciar(self, display_widget):
         if not self.evento:
@@ -22,12 +22,9 @@ class Cronometro:
         display_widget.text = '00:00:00:000'
 
     def atualizar(self, dt):
-
-        self.tempo_total += dt # delta tempo total, com parte decimal em milisegundos
-        self.segundos = int(self.tempo_total) # parte inteira do tempo total, ou seja os segundos
-
-        horas, resto = divmod(self.segundos, 3600) # extrai as horas
-        minutos, segundos = divmod(resto, 60) # extrai os minutos
-        milissegundos = int((self.tempo_total - self.segundos) * 1000) # extrai os milissegundos
-
+        self.tempo_total += dt
+        segundos_totais = int(self.tempo_total)
+        horas, resto = divmod(segundos_totais, 3600)
+        minutos, segundos = divmod(resto, 60)
+        milissegundos = int((self.tempo_total - segundos_totais) * 1000)
         self.display_widget.text = f"{horas:02}:{minutos:02}:{segundos:02}:{milissegundos:03}"
