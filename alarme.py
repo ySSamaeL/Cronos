49d2d2a5868fb4bb29b7b042af4b6a83d36c9d14
Samuel Pereira <124547921+ySSamaeL@users.noreply.github.com>
2025-02-12 11:36:34 -0300

    Update alarme.py

--- alarme.py
+++ alarme.py
@@ -2,6 +2,7 @@ from kivy.clock import Clock
 import datetime 
 from kivymd.uix.pickers import MDTimePicker
 from kivymd.uix.pickers import MDDatePicker
+from kivy.core.window import Window
 
 class Alarme:
     def __init__(self):
@@ -9,14 +10,30 @@ class Alarme:
         self.evento = None
         self.display_widget = None
 
-    def adicionar_alarme():
+    def adicionar_alarme(self):
         
-        dataescolhida = MDDatePicker()
-        dataescolhida.open()
+        date_dialog = MDDatePicker()
+        date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
+        date_dialog.open()
+        Window.size = (450, 751)
+        Window.size = (450, 750)
 
-        horaescolhida = MDTimePicker() 
-        horaescolhida.open()
+    def on_save(self, instance, value, date_range):
 
-        pass
+        '''
+        Events called when the "OK" dialog box button is clicked.
+
+        :type instance: <kivymd.uix.picker.MDDatePicker object>;
+        :param value: selected date;
+        :type value: <class 'datetime.date'>;
+        :param date_range: list of 'datetime.date' objects in the selected range;
+        :type date_range: <class 'list'>;
+        '''
+
+        print(instance, value, date_range)
+
+    def on_cancel(self, instance, value):
+        '''Events called when the "CANCEL" dialog box button is clicked.'''
+        
     
-    pass
+    
