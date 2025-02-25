ef294f8dac27ead7a2c23d608be51fccb0d52d3e
ySSamaeL <ssamuelkaka548@gmail.com>
2025-02-25 03:29:02 -0300

    edição finalizada

--- alarme.py
+++ alarme.py
@@ -9,10 +9,10 @@ import datetime
 from datetime import datetime, timedelta
 
 class Alarme:
-    def __init__(self, alarmes_list):
+    def __init__(self, alarmes_lista):
         self.alarmesativos = []
         self.evento = None
-        self.alarmes_list = alarmes_list
+        self.alarmes_lista = alarmes_lista
         self.data_selecionada = None
         self.hora_selecionada = None
 
@@ -92,18 +92,18 @@ class Alarme:
     def criar_alarme(self):
         if self.data_selecionada and self.hora_selecionada:
             # Cria um novo objeto Alarme
-            novo_alarme = Alarme(self.alarmes_list)
+            novo_alarme = Alarme(self.alarmes_lista)
             novo_alarme.data_selecionada = self.data_selecionada
             novo_alarme.hora_selecionada = self.hora_selecionada
             self.alarmesativos.append(novo_alarme)
             self.atualizar_lista_alarmes()
 
     def atualizar_lista_alarmes(self):
-        self.alarmes_list.clear_widgets()
+        self.alarmes_lista.clear_widgets()
         for alarme in self.alarmesativos:
             # Cria o item do alarme
             alarme_item = OneLineAvatarIconListItem(
-                IconLeftWidget(icon="pencil", on_release=lambda x: print("Edit clicked")),
+                IconLeftWidget(icon="pencil", on_release=self.criar_editar_alarme_callback(alarme)),
                 IconRightWidget(
                     icon="trash-can",
                     on_release=self.criar_remover_alarme_callback(alarme)
@@ -111,12 +111,85 @@ class Alarme:
                 text=str(alarme)
             )
             # Adiciona o item à lista de alarmes
-            self.alarmes_list.add_widget(alarme_item)
+            self.alarmes_lista.add_widget(alarme_item)
+
+    def criar_editar_alarme_callback(self, alarme):
+        def editar_alarme_callback(instance):
+            self.editar_alarme(alarme)
+        return editar_alarme_callback
 
     def criar_remover_alarme_callback(self, alarme):
         def remover_alarme_callback(instance):
             self.remover_alarme(alarme)
         return remover_alarme_callback
+
+    def editar_alarme(self, alarme):
+        # Abre o diálogo de edição para o alarme selecionado
+        self.data_selecionada = alarme.data_selecionada
+        self.hora_selecionada = alarme.hora_selecionada
+
+        date_dialog = MDDatePicker(min_date=datetime.now().date())
+        date_dialog.bind(on_save=lambda instance, value, date_range: self.salvar_data_edicao(instance, value, date_range, alarme), on_cancel=self.cancelar_data)
+        date_dialog.open()
+        Window.size = (450, 751)
+        Window.size = (450, 750)
+
+    def salvar_data_edicao(self, instance, value, date_range, alarme):
+        self.data_selecionada = value
+        if self.data_selecionada < datetime.now().date():
+            error_dialog = MDDialog(
+                title="Data Inválida",
+                text="Não é permitido selecionar uma data anterior à data atual.",
+                buttons=[
+                    MDFlatButton(
+                        text="OK",
+                        theme_text_color="Custom",
+                        text_color=MDApp.get_running_app().theme_cls.primary_color,
+                        on_release=lambda x: error_dialog.dismiss()
+                    )
+                ],
+            )
+            error_dialog.open()
+            return
+        print(f"Data selecionada: {self.data_selecionada}")
+        self.adicionarhora_edicao(alarme)
+
+    def adicionarhora_edicao(self, alarme):
+        time_dialog = MDTimePicker()
+        horaatual = (datetime.now() + timedelta(minutes=1)).time()
+        time_dialog.set_time(horaatual)
+        time_dialog.bind(on_save=lambda instance, value: self.salvar_hora_edicao(instance, value, alarme), on_cancel=self.cancelar_hora)
+        time_dialog.open()
+        Window.size = (450, 751)
+        Window.size = (450, 750)
+
+    def salvar_hora_edicao(self, instance, value, alarme):
+        self.hora_selecionada = value
+        horaminima = (datetime.now() + timedelta(minutes=1)).time()
+        print(self.hora_selecionada)
+        print(horaminima)
+        if self.data_selecionada == datetime.now().date() and self.hora_selecionada < datetime.now().time():
+            error_dialog = MDDialog(
+                title="Hora Inválida",
+                text="Não é permitido selecionar um horário anterior ao horário atual.",
+                buttons=[
+                    MDFlatButton(
+                        text="OK",
+                        theme_text_color="Custom",
+                        text_color=MDApp.get_running_app().theme_cls.primary_color,
+                        on_release=lambda x: error_dialog.dismiss()
+                    )
+                ],
+            )
+            error_dialog.open()
+            return
+        print(f"Hora selecionada: {self.hora_selecionada}")
+        self.atualizar_alarme(alarme)
+
+    def atualizar_alarme(self, alarme):
+        alarme.data_selecionada = self.data_selecionada
+        alarme.hora_selecionada = self.hora_selecionada
+        self.atualizar_lista_alarmes()
 
     def verificar_alarmes(self, dt):
         now = datetime.now()
@@ -127,10 +200,9 @@ class Alarme:
             if now >= alarm_time:
                 self.alarmar()
                 alarmesexpirados.append(alarme)
-                
     
         self.alarmesativos = [alarm for alarm in self.alarmesativos if alarm not in alarmesexpirados]
- 
+        
         
     def remover_alarme(self, alarme):
         # Remove o alarme da lista de alarmes ativos
